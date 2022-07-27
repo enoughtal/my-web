4c6913361fb11a2b3cdf49affefd4041c2ecb920
DanielFreeman <415919075@qq.com>
2022-07-27 20:17:28 +0800

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,25 +7,25 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 format: 'es',
-                manualChunks: (id) => {
-                    if (
-                        id.includes('node_modules/react/')
-                        || id.includes('node_modules/react-dom/')
-                        || id.includes('redux')
-                        || id.includes('react-router')
-                    ) {
-                        return 'react'
-                    }
-                    else if (
-                        id.includes('views/About')
-                        || id.includes('views/Blog')
-                        || id.includes('views/Login')
-                        || id.includes('views/Tictactoe')
-                        || id.includes('views/Todos')
-                    ) {
-                        return 'pages'
-                    }
-                },
+                //manualChunks: (id) => {
+                //    if (
+                //        id.includes('node_modules/react/')
+                //        || id.includes('node_modules/react-dom/')
+                //        || id.includes('redux')
+                //        || id.includes('react-router')
+                //    ) {
+                //        return 'react'
+                //    }
+                //    else if (
+                //        id.includes('views/About')
+                //        || id.includes('views/Blog')
+                //        || id.includes('views/Login')
+                //        || id.includes('views/Tictactoe')
+                //        || id.includes('views/Todos')
+                //    ) {
+                //        return 'pages'
+                //    }
+                //},
                 inlineDynamicImports: false
             },
         }
